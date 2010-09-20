e6d2c05c19f2929cb3b6e8b95771e7e0072addec
Ennder <mel@ennder.fr>
2010-09-20 13:17:45 +0200

    Ajout I178n à ConversionsWithI18n.to_s 	modified:   lib/translations/rails.rb

--- lib/translations/rails.rb
+++ lib/translations/rails.rb
@@ -1,4 +1,10 @@
 Rails.configuration.after_initialize do
 	I18n.load_path.unshift File.expand_path(File.join(File.dirname(__FILE__), 'locales', 'en.yml'))
 	I18n.load_path.unshift File.expand_path(File.join(File.dirname(__FILE__), 'locales', 'fr.yml'))
+
+	#JBA 2010-09-19
+	#pour que to_formatted_s prenne en compte les formats localisés
+	#  Date.send :include, ConversionsWithI18n
+	#  Time.send :include, ConversionsWithI18n
+	ActiveSupport::TimeWithZone.send :include, ConversionsWithI18n
 end
