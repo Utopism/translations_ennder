deff3e0e4dd3e3c2d4bb30573bab74ebc67ce864
Ennder <mel@ennder.fr>
2010-09-21 00:40:35 +0200

    modified:   lib/translations/conversions_with_i18n_label.rb

--- lib/translations/conversions_with_i18n_label.rb
+++ lib/translations/conversions_with_i18n_label.rb
@@ -6,30 +6,31 @@ module ConversionsWithI18nLabel
 			alias_method_chain :label, :i18n
 		end
 
-		def base.label_with_i18n_bold_and_colon(bold_and_colon, object_name, method, text = nil, options = {})
+	end
+
+	def label_with_i18n_bold_and_colon(bold_and_colon, object_name, method, text = nil, options = {})
 #			Rails.logger.debug "DEBUG JBA : label_with_i18n(#{object_name}, #{method}, #{text}, #{options})"
 
-			if !text.blank?
-				return label_without_i18n(object_name, method, text, options)
-			end
-
-			begin
-				_label_trans = I18n.t("activerecord.attributes.#{object_name.to_s.tableize.singularize}.#{method}", :raise => true)
-			rescue I18n::MissingTranslationData
-#				Rails.logger.debug "DEBUG JBA : traduction PAS trouvée(activerecord.attributes.#{object_name.to_s.tableize.singularize}.#{method})"
-				_label_trans = method
-			end
-
-#			Rails.logger.debug "DEBUG JBA : traduction =[#{_label_trans}]"
-			label_without_i18n(object_name, method, (bold_and_colon ? "<b>#{_label_trans}</b> :" : _label_trans), options)
+		if !text.blank?
+			return label_without_i18n(object_name, method, text, options)
 		end
 
-		def base.label_bold_and_colon(object_name, method, text = nil, options = {})
-			label_with_i18n_bold_and_colon(true, object_name, method, text, true, options)
+		begin
+			_label_trans = I18n.t("activerecord.attributes.#{object_name.to_s.tableize.singularize}.#{method}", :raise => true)
+		rescue I18n::MissingTranslationData
+#			Rails.logger.debug "DEBUG JBA : traduction PAS trouvée(activerecord.attributes.#{object_name.to_s.tableize.singularize}.#{method})"
+			_label_trans = method
 		end
+
+#		Rails.logger.debug "DEBUG JBA : traduction =[#{_label_trans}]"
+		label_without_i18n(object_name, method, (bold_and_colon ? "<b>#{_label_trans}</b> :" : _label_trans), options)
+	end
+
+	def label_bold_and_colon(object_name, method, text = nil, options = {})
+		self.label_with_i18n_bold_and_colon(true, object_name, method, text, true, options)
 	end
 
 	def label_with_i18n(object_name, method, text = nil, options = {})
-		label_with_i18n_bold_and_colon(false, object_name, method, text, options)
+		self.label_with_i18n_bold_and_colon(false, object_name, method, text, options)
 	end
 end
