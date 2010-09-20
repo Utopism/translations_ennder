c5f20eec11b3b82197ab2e3cd3d651fc777318ed
Ennder <mel@ennder.fr>
2010-09-20 16:59:32 +0200

    to_s et label séparés 	modified:   lib/translations.rb 	modified:   lib/translations/rails.rb

--- lib/translations.rb
+++ lib/translations.rb
@@ -1,2 +1,3 @@
 require 'translations/rails'
-require 'translations/conversions_with_i18n'
+require 'translations/conversions_with_i18n_to_s'
+require 'translations/conversions_with_i18n_label'
--- lib/translations/rails.rb
+++ lib/translations/rails.rb
@@ -5,9 +5,9 @@ Rails.configuration.after_initialize do
 	I18n.load_path.unshift File.expand_path(File.join(File.dirname(__FILE__), 'locales', 'fr.yml'))
 
 	#JBA 2010-09-19
-	#pour que to_formatted_s prenne en compte les formats localisés
-	#  Date.send :include, ConversionsWithI18n
-	#  Time.send :include, ConversionsWithI18n
-	ActiveSupport::TimeWithZone.send :include, ConversionsWithI18n
-	ActionView::Helpers::FormBuilder :include, ConversionsWithI18n
+	#pour que to_s prenne en compte les formats localisés
+	#  Date.send :include, ConversionsWithI18nToS
+	#  Time.send :include, ConversionsWithI18nToS
+	ActiveSupport::TimeWithZone.send :include, ConversionsWithI18nToS
+	ActionView::Helpers::FormBuilder :include, ConversionsWithI18nLabel
 end
