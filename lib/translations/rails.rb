7497e30651f0c2abd94efa9482e7f5d7a9c60247
Ennder <mel@ennder.fr>
2010-09-20 13:29:04 +0200

    Ajout trace 	modified:   lib/translations/rails.rb

--- lib/translations/rails.rb
+++ lib/translations/rails.rb
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 Rails.configuration.after_initialize do
 	I18n.load_path.unshift File.expand_path(File.join(File.dirname(__FILE__), 'locales', 'en.yml'))
 	I18n.load_path.unshift File.expand_path(File.join(File.dirname(__FILE__), 'locales', 'fr.yml'))
@@ -7,4 +8,5 @@ Rails.configuration.after_initialize do
 	#  Date.send :include, ConversionsWithI18n
 	#  Time.send :include, ConversionsWithI18n
 	ActiveSupport::TimeWithZone.send :include, ConversionsWithI18n
+    Rails.loggger.debug "ActiveSupport::TimeWithZone.to_s localisé"
 end
