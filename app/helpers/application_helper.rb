29b1f89b7f2c534cb6b18875ad518795e97705ff
Ennder <mel@ennder.fr>
2010-09-22 10:44:17 +0200

    bold_and_colon dans le helper de l'application method.humanize 	new file:   app/helpers/application_helper.rb 	modified:   lib/translations/conversions_with_i18n_label.rb

---
+++ app/helpers/application_helper.rb
@@ -0,0 +1,12 @@
+# Methods added to this helper will be available to all templates in the application.
+module ApplicationHelper
+	#Label de la forme : "<b><nom_colonne</b>: "
+	def label_bold_and_colon(object_name, method, text = nil, options = {})
+		Rails.logger.debug "DEBUG JBA : #{self.class.name}.label_bold_and_colon(#{object_name}, #{method}, #{text}, #{options})"
+		if text.blank?
+			label(true, object_name, method, method.humanize, true, options)
+		else
+			label(true, object_name, method, "<b>#{text}</b> :", true, options)
+		end
+	end
+end
--- lib/translations/conversions_with_i18n_label.rb
+++ lib/translations/conversions_with_i18n_label.rb
@@ -9,8 +9,8 @@ module ConversionsWithI18nLabel
 
 	end
 
-	def label_with_i18n_bold_and_colon(bold_and_colon, object_name, method, text = nil, options = {})
-		Rails.logger.debug "DEBUG JBA : #{self.class.name}.label_with_i18n_bold_and_colon(#{object_name}, #{method}, #{text}, #{options})"
+	def label_with_i18n(object_name, method, text = nil, options = {})
+		Rails.logger.debug "DEBUG JBA : #{self.class.name}.label_with_i18n(#{object_name}, #{method}, #{text}, #{options})"
 
 		if !text.blank?
 			return label_without_i18n(object_name, method, text, options)
@@ -24,21 +24,6 @@ module ConversionsWithI18nLabel
 		end
 
 #		Rails.logger.debug "DEBUG JBA : traduction =[#{_label_trans}]"
-		label_without_i18n(object_name, method, (bold_and_colon ? "<b>#{_label_trans}</b> :" : _label_trans), options)
-	end
-
-	def label_bold_and_colon(object_name, method, text = nil, options = {})
-		Rails.logger.debug "DEBUG JBA : #{self.class.name}.label_bold_and_colon(#{object_name}, #{method}, #{text}, #{options})"
-		label_with_i18n_bold_and_colon(true, object_name, method, text, true, options)
-	end
-
-	def label_with_i18n(object_name, method, text = nil, options = {})
-		Rails.logger.debug "DEBUG JBA : #{self.class.name}.label_with_i18n(#{object_name}, #{method}, #{text}, #{options})"
-
-		self.class.methods.sort.each do | _method |
-			Rails.logger.debug "DEBUG JBA : #{_method}"
-		end
-
-		label_with_i18n_bold_and_colon(false, object_name, method, text, options)
+		label_without_i18n(object_name, method, _label_trans, options)
 	end
 end
