6e37ae2136c970ec717af80c948f85c55af2a5c5
Ennder <mel@ennder.fr>
2011-01-07 18:20:54 +0100

    Plugin => Gem 	deleted:    MIT-LICENSE 	deleted:    README 	deleted:    lib/translations/rails.rb 	deleted:    rails/init.rb 	deleted:    test/test_helper.rb 	deleted:    test/translations_ennder_test.rb

--- rails/init.rb
+++
@@ -1,5 +0,0 @@
-# Include hook code here
-
-require 'translations'
-#Now you can add any ‘require’ statements to ‘lib/translations.rb’ and keep ‘init.rb’ clean
-
--- test/translations_ennder_test.rb
+++
@@ -1,8 +0,0 @@
-require 'test_helper'
-
-class TranslationsEnnderTest < ActiveSupport::TestCase
-	test "the truth" do
-		#TODO test apport traduction
-		assert true
-	end
-end
