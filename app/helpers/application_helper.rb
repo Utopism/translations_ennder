6aa6da8d382bea2aa5067ba66b4e150c3f11a06d
Ennder <mel@ennder.fr>
2011-01-07 17:35:50 +0100

    Correction '>' manquant 	modified:   app/helpers/application_helper.rb

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,6 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
-	#Label de la forme : "<b><nom_colonne</b>: "
+	#Label de la forme : "<b><nom_colonne></b>: "
 	def label_bold_and_colon(object_name, method, text = nil, options = {})
 		Rails.logger.debug "DEBUG JBA : #{self.class.name}.label_bold_and_colon(#{object_name}, #{method}, #{text}, #{options})"
 		if text.blank?
