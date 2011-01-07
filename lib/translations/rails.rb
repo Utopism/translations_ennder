00e2ba988b99db67a10b979e59354ce37c24639d
Ennder <mel@ennder.fr>
2011-01-07 17:36:54 +0100

    commentaire impact Rails 3 	modified:   lib/translations/rails.rb

--- lib/translations/rails.rb
+++ lib/translations/rails.rb
@@ -4,7 +4,7 @@ Rails.configuration.after_initialize do
 	I18n.load_path.unshift File.expand_path(File.join(File.dirname(__FILE__), 'locales', 'en.yml'))
 	I18n.load_path.unshift File.expand_path(File.join(File.dirname(__FILE__), 'locales', 'fr.yml'))
 
-	#JBA 2010-09-19
+	#JBA 2010-09-19, sera caduque avec Rails 3
 	#pour que to_s prenne en compte les formats localisés
 	#  Date.send :include, ConversionsWithI18nToS
 	#  Time.send :include, ConversionsWithI18nToS
