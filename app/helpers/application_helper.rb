00b9ebf3c0b2dfd5733aa5b04196501eda0efbcc
Ennder <mel@ennder.fr>
2011-01-10 16:20:00 +0100

    Ajout set_locale au helper de l'appli 	modified:   app/helpers/application_helper.rb

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -9,4 +9,36 @@ module ApplicationHelper
 			label(true, object_name, method, "<b>#{text}</b> :", true, options)
 		end
 	end
+
+	########################################################
+	# Recherche et M-À-J la locale						   #
+	# depuis les params de requête, la session, la requête #
+	# JBA 2010-09-07									   #
+	########################################################
+	def set_locale
+		if (params[:locale] and params[:locale].match /^(en|fr)$/)
+			I18n.locale = params[:locale]
+		elsif session[:locale].nil?
+			if request.env['HTTP_ACCEPT_LANGUAGE'].blank?
+				#2011-01-05 Vérif° supplémentaire
+				_http_lang = ''
+			else
+				_http_lang = request.env['HTTP_ACCEPT_LANGUAGE'].scan(/^[a-z]{2}/).first
+			end
+
+			if _http_lang.match /^(en|fr)$/
+				I18n.locale = _http_lang
+			else
+				I18n.locale = :fr
+			end
+		else
+			I18n.locale = session[:locale] || :fr
+		end
+
+#		logger.debug "locale=[#{session[:locale]}]"
+
+		if session[:locale] != I18n.locale
+			session[:locale] = I18n.locale
+		end
+	end
 end
